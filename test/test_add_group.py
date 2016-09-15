2ddcfe9bdd0026ac480f6ff37b32f3e7d6e5bd24
nechepurenko <wwwolf@ukr.net>
2016-09-15 22:32:38 +0300

    grouphelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="test_name", header="test_header", footer="test_footer"))
+    app.group.create(Group(name="test_name", header="test_header", footer="test_footer"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
