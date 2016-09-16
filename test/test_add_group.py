5979ad81108eadea95823557bd022de05af21b6c
nechepurenko <wwwolf@ukr.net>
2016-09-16 10:23:56 +0300

    if else

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,12 +6,10 @@ from model.group import  Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="test_name", header="test_header", footer="test_footer"))
-    app.session.logout()
+
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
+
