633e16a81e9755de34bea9d77bf0f96926d65547
nechepurenko <wwwolf@ukr.net>
2016-09-16 15:54:16 +0300

    all 3 lessons

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,8 +1,16 @@
 from model.group import  Group
 
 def test_modify_group_name(app):
+    if_exist_group(app)
     app.group.modify_first_group(Group(name="New group"))
 
 
+
 def test_modify_group_header(app):
+    if_exist_group(app)
     app.group.modify_first_group(Group(header="New header"))
+
+
+def if_exist_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
