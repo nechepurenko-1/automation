a2491e5bbb3a6a3dd07593747cfa3b2a7def0c13
nechepurenko <wwwolf@ukr.net>
2016-09-16 16:40:36 +0300

    lesson 4.1

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,10 +6,15 @@ from model.group import  Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="test_name", header="test_header", footer="test_footer"))
-
+    new_groups = app.group.get_group_list()
+    assert  len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
